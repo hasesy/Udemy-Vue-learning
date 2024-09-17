9dc2efcab76c47158f9ca3f519c9b5dccc287f48
Seoyeon Kim <kseoy0607@gmail.com>
2024-09-17 12:51:36 +0900

    240917 외부 파일 import / export 해서 가져오기 및 내보내기

--- src/App.vue
+++ src/App.vue
@@ -26,35 +26,14 @@
 </template>
 
 <script>
+import data from './assets/movies';
+
 export default {
   name: 'App',
   data() {
     return {
       isModal: false,
-      data: [
-        {
-          title: '노량',
-          year: 2023,
-          category: '액션, 드라마',
-          textRed: 'color: red',
-          like: 0,
-          imgUrl: '/assets/노량.webp',
-        },
-        {
-          title: '아쿠아맨과 로스트 킹덤',
-          year: 2023,
-          category: '액션, 판타지, 어드벤처',
-          like: 0,
-          imgUrl: '/assets/아쿠아맨.webp',
-        },
-        {
-          title: '3일의 휴가',
-          year: 2023,
-          category: '판타지, 드라마',
-          like: 0,
-          imgUrl: '/assets/3일의휴가.webp',
-        },
-      ],
+      data: data,
     };
   },
   methods: {
