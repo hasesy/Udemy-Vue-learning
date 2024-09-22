94026e97fc1fc8c8823731f9dc29291ecf7d257e
Seoyeon Kim <kseoy0607@gmail.com>
2024-09-22 23:56:43 +0900

    240922 2. 검색창 입력 값 검사 watch, @change

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="search-box">
-    <input type="search" v-model="inputText" placeholder="검색어 입력" />
+    <input
+      type="search"
+      @change="inputText = $event.target.value"
+      placeholder="검색어 입력"
+    />
     <button>검색</button>
   </div>
   {{ inputText }}
@@ -13,6 +17,14 @@ export default {
     return {
       inputText: '',
     };
+  },
+  watch: {
+    inputText(name) {
+      // 입력한 영화제목이 데이터에 있는지 확인
+      if (name !== '노량') {
+        alert('해당하는 영화가 없습니다.');
+      }
+    },
   },
 };
 </script>
